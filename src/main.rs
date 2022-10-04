fc97e8fb69344f274329e84c4f082fdfca06718e
SKTT1Ryze <linuxgnulover@gmail.com>
2022-10-04 14:32:07 +0800

    succeed in interacting with neos-ts

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::{net::SocketAddr, str::FromStr};
 use warp::Filter;
 
-const ADDR: &str = "127.0.0.1:3000";
+const ADDR: &str = "127.0.0.1:3030";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     pretty_env_logger::try_init()?;
 
-    let hello = warp::path!("hello" / String).map(|name| format!("hello, {}", name));
+    // todo: use CORS correctly
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_credentials(true)
+        .allow_header("content-type")
+        .allow_method("GET");
+
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("hello, {}", name))
+        .with(cors);
 
     warp::serve(hello).run(SocketAddr::from_str(ADDR)?).await;
 
